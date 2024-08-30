973bea92a2d4de9df26f340d3e1e3ff33e25cb01
anasmalki <97480216+anasmalki@users.noreply.github.com>
2024-08-30 20:57:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nmrfit',
-    version='0.1',
+    version='0.5',
     packages=find_packages(),
     install_requires=[
         'numpy',
